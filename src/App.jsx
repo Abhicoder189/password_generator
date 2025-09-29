c3be6fa29e76c4ac4e071b18fde06aa31900dc0a
Abhicoder189 <johndoe@example.com>
2025-09-29 21:09:43 +0530

    First Commit

---
+++ src/App.jsx
@@ -0,0 +1,58 @@
+import { useState,useCallback,useEffect } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [length, setLength] = useState(8)
+  const [numberAllowed, setNumberAllowed] = useState(false)
+  const [charAllowed, setCharAllowed] = useState(false)
+  const [password, setPassword] = useState('')
+  const genratePassword = useCallback(()=>{
+    let  pass ="";
+    let str ="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+    if(numberAllowed) str += "0123456789"
+    if(charAllowed) str += "!@#$%^&*()_+"
+    for(let i =0;i<length;i++){
+    const char =  Math.floor((Math.random()*str.length)+1)
+    pass +=str.charAt(char)
+    }
+   
+    setPassword(pass)
+  },[length,numberAllowed,charAllowed])
+   const copyPassword =()=>{
+      window.navigator.clipboard.writeText(password)
+    }
+  useEffect(()=>{
+    genratePassword()
+  },[length,numberAllowed,charAllowed])
+
+  return (
+    <>
+     <h1 className='bg-red-500'>Password Genrator</h1>
+     <div className=''>
+      <input type="text" name="" value={password} className='' placeholder='password' readOnly id="" />
+      <button className=''onClick={copyPassword}>Copy</button>
+
+     </div>
+      <div className=''>
+        <div className=''>
+          <input type="range" min={6} max={100} value={length} className='cursor-pointer' onChange={(e)=>setLength(e.target.value)} name="" id="" />
+          <label htmlFor="length">Length: {length}</label>
+        </div>
+         <div className=''>
+          <input type="checkbox" defaultChecked={numberAllowed} onChange={()=>{setNumberAllowed((prev)=>!prev)}} className='cursor-pointer'  name="" id="" />
+          <label htmlFor="number">Number</label>
+
+        </div>
+        <div className=''>
+          <input type="checkbox" defaultChecked={charAllowed} onChange={()=>{setCharAllowed((prev)=>!prev)}}   className='cursor-pointer'  name="" id="" />
+          <label htmlFor="char">Character</label>
+          
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
